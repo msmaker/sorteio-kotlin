63086ba6f41e0c9804011333773f0e2c812b3fd3
msMaker <marcosilva.tec@gmail.com>
2020-05-02 12:58:02 -0300

    Simple version

---
+++ app/src/main/java/com/msmaker/myapplicationk/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.msmaker.myapplicationk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View) {
+
+        var texto = findViewById<TextView>(R.id.textViewSorteio)
+        var numeroSorteado = Random().nextInt(11)
+
+        //texto.setText("Numero sorteado é: ")
+        texto.text = "Numero sorteado é: " + numeroSorteado
+
+    }
+
+}
